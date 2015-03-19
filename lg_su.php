65088bc279544aabec6ec3d57af518f2efe51056
piyush kedia <piyushkedia1994@gmail.com>
2015-03-19 20:46:54 +0530

    afte login page

---
+++ lg_su.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name "viewport" content="width=device-width,initial-scale-1.0">
+    <title>MNNIT-conference</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+     
+  
+</head>
+<body>
+  <div class="page-header no-margin row">
+    
+    <div class="col-md-2"><img src="images/mnnit1.png" height="90px" class="pull-left"></div>
+    <div class="col-md-10"><h2 style="font-size:40px;"><strong>Motilal Nehru National Institute Of Technology</strong></h2></div>
+  
+</div>
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+  <div class ="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-mnnit-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+          <a class="navbar-brand " href="#">Conference Login</a>
+  </div>
+  
+
+  <div class="collapse navbar-collapse navbar-right" id="bs-mnnit-navbar-collapse-1">
+    
+ 
+    <ul class="nav nav-pills btn navbar-btn ">
+
+  <li role="presentation"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Submissions</a></li>
+  <li role="presentation"><a href="#">conferences</a></li>
+  <li role="presentation" ><a href="#">status</a></li>
+  
+  
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
+      Reviews <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu" role="menu">
+      <li><a href="#">My Papers</a></li>
+      <li><a href="#">All Papers</a></li>
+      <li><a href="#">Delete</a></li>
+      <li><a href="#">Send for Review</a></li>
+    </ul>
+  </li>
+  
+</ul>
+</div>
+
+</div>
+</nav>
+
+
+
+
+  
+  <div class="container">
+    <h4>Conference-Info</h4>
+
+
+<div class="col-md-4 ">
+  <table class="table table-bordered">
+    <tbody>
+  <tr>
+    <th>Name of a event</th>
+    <td>ICCCT-2015</td>
+    
+  </tr>
+  <tr>
+    <th>Website</th>
+    <td><a href="www.mnnit.ac.in">www.mnnnit.ac.in</td>
+  </tr>
+</tr>
+  <tr>
+    <th>Submission Page</th>
+    <td><a href="fillform.php">mnnit.ac.in/submission</td>
+  </tr>
+
+
+
+
+    </tbody>
+    </table>
+  </div>
+
+<br><br><br><br><br><br><br>
+
+
+
+
+    <h4>Important Dates</h4>
+    <div class="col-md-4 ">
+  <table class="table table-bordered">
+    <tbody>
+  <tr>
+    <th>Description</th>
+    <th>Dates</th>
+    
+  </tr>
+  <tr>
+    <td>Iccct-2015</td>
+    <td>2015-09-25 - 2015-09-27</td>
+  </tr>
+
+
+
+
+    </tbody>
+    </table>
+  </div>
+</div>
+
+
+
+
+
+<footer class="site-footer navbar-fixed-bottom no-margin">
+  <div class="container">
+    <div class="row">
+    <p>© 2015 All Rights Reserved</p>
+    </div>
+
+    <div class="bottom-footer navbar-static-bottom">
+      <div class="cpl-md-5"></div>
+      <div class="cpl-md-6">
+        <ul class="footer-nav">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="link.html">Link</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+
+
+</html> 
